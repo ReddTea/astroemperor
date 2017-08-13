cfcaf45eb97dd4a4094f252d4a00218789f64ba4
Pablo Aníbal Peña Rojas <pablo.pena@ing.uchile.cl>
2017-08-13 18:50:42 -0300

    Update and rename test_emperor.py to test_astroemperor.py

--- test_astroemperor.py
+++ test_astroemperor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import emperor
+import astroemperor
 import scipy as sp
 
 # SETUP
@@ -14,7 +14,7 @@ setup = sp.array([2, 100, 260])  # temperatures, walkers, steps
 # Constrains for the signals (k>0), they use the change of variable so don't try to make much of them!
 # The format is sp.array([As_0, As_1, log(Period0), log(Period1), Ac_0, Ac_1, S_0, S_1, C_0, S_1]),
 # each file is a signal! Any value set as -sp.inf gets the automatic constrain
-# made by emperor.
+# made by astroemperor.
 # A really short test chain verify everything is working OK, so we input some boundaries
 # arround the actual values we got with a longer chain.
 
@@ -22,7 +22,7 @@ BOUNDARY = sp.array([[-1.01515928e+01, -6.33312469e+00, 4.11098843e+00, 4.111054
                      [-5.69294095e+00, 6.05896817e-02, 3.40831451e+00, 3.40863545e+00, -9.33328013e+00, -8.07370401e+00,  -2.48303912e-01,  -7.10641857e-02, 3.47811764e-02,   1.79877743e-01]])
 
 # We initialize the code
-em = emperor.EMPIRE(stardat, setup)  # EMPIRE(data_to_read, chain_parameters)
+em = astroemperor.EMPIRE(stardat, setup)  # EMPIRE(data_to_read, chain_parameters)
 
 # We configure the settings
 em.CORNER = False  # corner plot disabled as it takes some time to plot
@@ -44,7 +44,7 @@ print('Everything is working fine !! Why dont you try running a longer chain wit
 stardat = sp.array(['GJ876_1_LICK.vels', 'GJ876_2_KECK.vels'])  # same data
 setup = sp.array([3, 150, 5000])  # ntemps, nwalkers, nsteps, now real
 
-em = emperor.EMPIRE(stardat, setup)  # EMPIRE(data_to_read, chain_parameters)
+em = astroemperor.EMPIRE(stardat, setup)  # EMPIRE(data_to_read, chain_parameters)
 em.STARMASS = 0.37  # known mass for this particular star GJ876
 em.CORNER = False  # corner plot disabled
 em.eccprior = 0.1  # sigma for the eccentricity prior!
