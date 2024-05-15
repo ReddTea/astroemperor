474707c8d4aa95a991c8324500cdf6199557709e
ReddTea <anibalrojas@outlook.com>
2024-05-15 17:14:37 -0400

    terminal width colab fix

--- src/astroemperor/globals.py
+++ src/astroemperor/globals.py
@@ -10,6 +10,11 @@ _PLATFORM_SYSTEM = platform.system()  # PROBABLY BEST, should bench
 _SYS_PLATFORM = sys.platform
 _OS_NAME = os.name
 _CORES = multiprocessing.cpu_count()
-_TERMINAL_WIDTH = os.get_terminal_size().columns
 _OS_ROOT = os.path.dirname(__file__)  # where im now
+try:
+    _TERMINAL_WIDTH = os.get_terminal_size().columns
+except:
+    print('I couldnt grab the terminal size! Trying with pandas...')
+    from pandas import get_option
+    _TERMINAL_WIDTH = get_option('display.width')
         
